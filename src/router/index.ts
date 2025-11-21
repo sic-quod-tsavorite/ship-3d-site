27f8a0c6b5819837e7fa41cb002cbf06575b2dea
Tsavorite <56306066+sic-quod-tsavorite@users.noreply.github.com>
2025-11-21 09:53:04 +0100

    Build cmd and path prefix for online dev

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,26 +7,28 @@ import LoginView from "@/views/admin/LoginView.vue";
 import NotFound from "@/views/NotFound.vue";
 import { useAuthStore } from "@/stores/auth";
 
+const routePrefix = import.meta.env.VITE_ROUTER_PREFIX || "";
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
+      path: `${routePrefix}/`,
       name: "home",
       component: HomeView as Component,
     },
     {
-      path: "/map",
+      path: `${routePrefix}/map`,
       name: "map",
       component: MapView as Component,
     },
     {
-      path: "/login",
+      path: `${routePrefix}/login`,
       name: "login",
       component: LoginView as Component,
     },
     {
-      path: "/admin",
+      path: `${routePrefix}/admin`,
       name: "admin",
       component: AdminView as Component,
       meta: { requiresAuth: true },
