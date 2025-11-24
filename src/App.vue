d4ee32a8ab0932766b9cc1e4037d6838ad0ad372
Tsavorite <56306066+sic-quod-tsavorite@users.noreply.github.com>
2025-11-24 11:43:27 +0100

    Navigation => dev || admin only

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col min-h-screen bg-gray-100 text-gray-800">
-    <TheNavigation />
+    <TheNavigation v-if="showNavigation" />
     <main class="grow container mx-auto p-4">
       <RouterView />
     </main>
@@ -11,10 +11,20 @@
 <script setup lang="ts">
 // Imports
 import { RouterView } from "vue-router";
+import { computed } from "vue";
+import { storeToRefs } from "pinia";
 
 // Project imports
 import TheNavigation from "./components/TheNavigation.vue";
 import TheFooter from "./components/TheFooter.vue";
+import { useAuthStore } from "@/stores/auth";
+
+const auth = useAuthStore();
+const { isLoggedIn } = storeToRefs(auth);
+
+const showNavigation = computed<boolean>(
+  () => import.meta.env.DEV || isLoggedIn.value
+);
 </script>
 
 <style lang="scss" scoped></style>
